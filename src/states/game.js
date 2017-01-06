07a2def3b6fb04c73da35dc2d076874d1392aa64
ddmills <mills.dalton@gmail.com>
2017-01-05 21:41:38 -0600

    add game cursor

--- src/states/game.js
+++ src/states/game.js
@@ -5,5 +5,18 @@ export default class Game extends Phaser.State {
 
     this.backgroundLayer = this.map.createLayer('background');
     this.backgroundLayer.resizeWorld();
+
+    this.cursor = this.game.add.graphics();
+    this.cursor.lineStyle(1, 0xff9ae6, 1);
+    this.cursor.drawRect(0, 0, 16, 16);
+
+    this.game.input.addMoveCallback(this.updateCursor, this);
+  }
+
+  updateCursor() {
+    const mouseX = this.game.input.activePointer.worldX;
+    const mouseY = this.game.input.activePointer.worldY;
+    this.cursor.x = this.backgroundLayer.getTileX(mouseX) * 16;
+    this.cursor.y = this.backgroundLayer.getTileY(mouseY) * 16;
   }
 };
