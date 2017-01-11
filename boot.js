ee55a170c32700a946ba76b6faaa5283718e2a9d
ddmills <mills.dalton@gmail.com>
2017-01-10 18:33:01 -0600

    Update 2017-01-11T00:32:59.267Z

--- boot.js
+++ boot.js
@@ -1,6 +1,8 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
+require('./phaser/bootstrap');
+
 var _preload = require('./states/preload');
 
 var _preload2 = _interopRequireDefault(_preload);
@@ -44,7 +46,74 @@ var Boot = function (_Phaser$Game) {
 
 new Boot();
 
-},{"./states/game":3,"./states/loading":4,"./states/preload":5}],2:[function(require,module,exports){
+},{"./phaser/bootstrap":2,"./states/game":5,"./states/loading":6,"./states/preload":7}],2:[function(require,module,exports){
+'use strict';
+
+var _tilemap = require('./tilemap');
+
+var _tilemap2 = _interopRequireDefault(_tilemap);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+Phaser.Tilemap = _tilemap2.default;
+
+},{"./tilemap":3}],3:[function(require,module,exports){
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var Tilemap = function (_Phaser$Tilemap) {
+  _inherits(Tilemap, _Phaser$Tilemap);
+
+  function Tilemap() {
+    _classCallCheck(this, Tilemap);
+
+    return _possibleConstructorReturn(this, (Tilemap.__proto__ || Object.getPrototypeOf(Tilemap)).apply(this, arguments));
+  }
+
+  _createClass(Tilemap, [{
+    key: "binarySum",
+    value: function binarySum(centerX, centerY, layer, test) {
+      var sum = 0;
+
+      var multiplier = 256;
+      for (var i = -1; i <= 1; i++) {
+        var y = centerY - i;
+
+        for (var j = -1; j <= 1; j++) {
+          if (i === 0 && j === 0) continue;
+          var x = centerX - j;
+
+          var tile = this.getTile(x, y, layer);
+
+          if (test(tile)) {
+            sum += multiplier;
+          }
+
+          multiplier /= 2;
+        }
+      }
+
+      return sum;
+    }
+  }]);
+
+  return Tilemap;
+}(Phaser.Tilemap);
+
+exports.default = Tilemap;
+
+},{}],4:[function(require,module,exports){
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
@@ -84,7 +153,7 @@ var Tower = function (_Phaser$TileSprite) {
 
 exports.default = Tower;
 
-},{}],3:[function(require,module,exports){
+},{}],5:[function(require,module,exports){
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
@@ -125,9 +194,9 @@ var Game = function (_Phaser$State) {
 
       this.buildings = this.game.add.group();
 
-      this.cursor = this.game.add.graphics();
-      this.cursor.lineStyle(1, 0xd698c7, 1);
-      this.cursor.drawRect(0, 0, 16, 16);
+      this.cursor = this.game.add.sprite(32, 32, 'cursor');
+      this.cursor.animations.add('spin');
+      this.cursor.animations.play('spin', 15, true);
 
       this.game.input.addMoveCallback(this.updateCursor, this);
     }
@@ -144,7 +213,14 @@ var Game = function (_Phaser$State) {
 
       if (this.game.input.mousePointer.isDown) {
         var tile = this.map.getTile(tileX, tileY, 'background');
+
         if (_tower2.default.canBePlacedAt(tile)) {
+          var sum = this.map.binarySum(tileX, tileY, 'background', function (t) {
+            return !!t.properties.buildable;
+          });
+
+          // const sum = tile.binarySum(t => !!t.properties.buildable);
+          console.log('BIN SUM', sum);
           this.placeTower(tile);
         }
       }
@@ -164,7 +240,7 @@ var Game = function (_Phaser$State) {
 exports.default = Game;
 ;
 
-},{"../prefabs/tower":2}],4:[function(require,module,exports){
+},{"../prefabs/tower":4}],6:[function(require,module,exports){
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
@@ -196,11 +272,13 @@ var Loading = function (_Phaser$State) {
       this.loadingBar.anchor.setTo(0.5);
       this.load.setPreloadSprite(this.loadingBar);
 
-      this.load.tilemap('island', 'maps/island.json', null, Phaser.Tilemap.TILED_JSON);
       this.load.image('tile-ground', 'img/ground.png');
-
       this.load.image('tower', 'img/tower.png');
       this.load.image('flag', 'img/flag.png');
+
+      this.load.tilemap('island', 'maps/island.json', null, Phaser.Tilemap.TILED_JSON);
+
+      this.load.spritesheet('cursor', 'img/cursor.png', 16, 16);
     }
   }, {
     key: 'create',
@@ -217,7 +295,7 @@ var Loading = function (_Phaser$State) {
 exports.default = Loading;
 ;
 
-},{}],5:[function(require,module,exports){
+},{}],7:[function(require,module,exports){
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
